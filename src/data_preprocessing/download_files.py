4262d4af08a8f942ff261a615416c770cb8991c3
Aldo Sandoval <asandoval@ndscognitivelabs.com>
2024-08-16 19:47:22 -0600

    Script for file download completed and data catalog reviewed.

--- src/data_preprocessing/download_files.py
+++ src/data_preprocessing/download_files.py
@@ -3,6 +3,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import pandas as pd
 import requests
+from loguru import logger
 
 
 def download_pdf(url, filename, folder="data/raw/"):
@@ -13,23 +14,29 @@ def download_pdf(url, filename, folder="data/raw/"):
         filepath = os.path.join(folder, filename)
         with open(filepath, "wb") as f:
             f.write(response.content)
-        print(f"Downloaded: {filename}")
+        logger.info(f"Downloaded: {filename}")
     except requests.exceptions.RequestException as e:
-        print(f"Failed to download {filename}: {e}")
+        logger.error(f"Failed to download {filename}: {e}")
 
 
 def download_pdfs_from_csv(csv_file, max_workers=os.cpu_count()):
     df = pd.read_csv(csv_file)
-
+    downloads = 0
+    logger.warning('Downloading files from "ojp.puebla.gob.mx" ...')
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = []
         for _, row in df.iterrows():
-            url = row["url"]
-            filename = row["filename"]
-            futures.append(executor.submit(download_pdf, url, filename))
+            if row["download"]:
+                url = row["url"]
+                filename = row["filename"]
+                folder = f'data/raw/{row["doc_type"]}'
+                futures.append(executor.submit(download_pdf, url, filename, folder))
 
         for future in as_completed(futures):
             future.result()
+            downloads += 1
+
+    logger.success(f"{downloads} files has been downloaded successfully.")
 
 
 if __name__ == "__main__":
